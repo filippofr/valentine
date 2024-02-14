c950668c8f0b3bdf2eb37c15d86da99f85bee765
Filippofr <filippo.fr7@gmail.com>
2024-02-14 14:26:17 +0100

    happy valentine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'valentine';
+  style = {};
+  
+  url1 = "../assets/images/love-cat.gif";
+  url2 = "../assets/images/love2-cat.gif";
+
+  imgurl = this.url1;
+
+  yesClicked = false;
+
+  changePosition() {
+    var x = Math.floor(Math.random() * 100);
+    var y = Math.floor(Math.random() * 100);
+    this.style = { top: y + 'vh', left: x + 'vw' };
+  }
+
+  accept() {
+    this.yesClicked = true;
+    this.imgurl = this.url2;
+  }
 }
